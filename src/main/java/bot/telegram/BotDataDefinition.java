b0c5421e2122f7cad4b573caaacc87effa3c70be
10dcar <daniel.carbunescu@gmail.com>
2025-04-01 19:43:16 +0200

    reverse code for checks

--- src/main/java/bot/telegram/BotDataDefinition.java
+++ src/main/java/bot/telegram/BotDataDefinition.java
@@ -33,11 +33,12 @@ public class BotDataDefinition {
                 String anh = "Healthy name >" + node.getScanner_address() +">"+node.getScanner_name() + " " + fortaScore + "\n";
                 boolean allHealthy = false;
                 try {
-                    if (Float.compare(Float.parseFloat(fortaScore), entry.getValue().getTrigger_value()) < 0){
+                    /*if (Float.compare(Float.parseFloat(fortaScore), entry.getValue().getTrigger_value()) < 0){
                         allHealthy = false;
                     } else {
                         allHealthy = true;
-                    }
+                    }*/
+                    allHealthy = (Float.compare(Float.parseFloat(fortaScore), entry.getValue().getTrigger_value()) < 0);
                 } catch (NumberFormatException e) {
                     System.out.println("BotDataDefinition.java - NumberFormatException");
                 }
