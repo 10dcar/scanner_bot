0b01ccd1d167b2a758d502f85af993cadb6b4b60
enatech_home <daniel.carbunescu@gmail.com>
2025-10-21 19:22:44 +0200

    update to telegram api

--- src/main/java/bot/telegram/PollingBot.java
+++ src/main/java/bot/telegram/PollingBot.java
@@ -32,7 +32,7 @@ public class PollingBot {
         while (true) {
             System.out.println("Starting polling next...");
             try {
-                String getUpdatesUrl = "https://api.telegram.org/bot" + botToken + "/getUpdates?timeout=10&allowed_updates=true"
+                String getUpdatesUrl = "https://api.telegram.org/bot" + botToken + "/getUpdates?timeout=30&allowed_updates=true"
                         + (offset > 0 ? "&offset=" + offset : "");
 
                 System.out.println("Starting polling url..." + getUpdatesUrl);
